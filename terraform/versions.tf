dc61a4a026b44c43364b3bc08900e330d6630481
Jizu <jizu.sun@philips.com>
2026-06-03 15:00:08 +0800

    fix: format versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -21,10 +21,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "backstage-platform-tfstate"
-    key            = "infrastructure/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "backstage-platform-tfstate"
+    key          = "infrastructure/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
